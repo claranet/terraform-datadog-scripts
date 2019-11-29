344a70cecc47898fcf1a0e081059a5e5a24d80ce
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2019-11-29 18:36:14 +0100

    MON-541 check for variable instead of hardcoded values

--- 90_best_practices.sh
+++ 90_best_practices.sh
@@ -12,8 +12,8 @@ echo -e "\t- Check only one notify_no_data set to true per module"
 # loop over every modules
 for module in $(browse_modules "$(get_scope ${1:-})" "${REPO}-*.tf"); do
     # check if there is more than 1 notify_no_data parameter set to true per set of monitors
-    if [[ $(cat ${module}/monitors-*.tf | grep -c notify_no_data.*true) -gt 1 ]]; then 
-        echo "More than one notify_no_data set to true on $module"
+    if [[ $(cat ${module}/monitors-*.tf | grep -c notify_no_data.*var\.*) -gt 1 ]]; then 
+        echo "More than one notify_no_data set with variable on $module"
         exit 1
     fi
 done
