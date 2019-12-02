63a06644a19ba6c474618892f79a3d869928dbaa
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2019-12-02 11:06:33 +0100

    MON-451 check for not false notify no data instead of variable

--- 90_best_practices.sh
+++ 90_best_practices.sh
@@ -11,11 +11,11 @@ echo "Check best practices respect"
 echo -e "\t- Check only one notify_no_data set to true per module"
 # loop over every modules
 for module in $(browse_modules "$(get_scope ${1:-})" "${REPO}-*.tf"); do
-    # check if there is more than 1 notify_no_data parameter set from variable (and possibly to true).
-    # here the risk to check is "true" value only while this could lead the duplicated no data alerts.
+    # check if there is more than 1 notify_no_data not set to false.
+    # here the risk to check is the possible "true" value while this could lead the duplicated no data alerts.
     # every metrics from one module come from the same source so this does not make sens to check no data for multiple monitors in the same module.
-    if [[ $(cat ${module}/monitors-*.tf | grep -c notify_no_data.*var\.*) -gt 1 ]]; then 
-        echo "More than one notify_no_data set with variable on $module"
+    if [[ $(cat ${module}/monitors-*.tf | grep 'notify_no_data[[:space:]]*=' | grep -cv '=[[:space:]]*false') -gt 1 ]]; then 
+        echo "More than one notify_no_data not set to \"false\" in $module"
         exit 1
     fi
 done
