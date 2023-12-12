853fc010d98bad3b80102c1407ff881f10cdd78e
Matthieu Bourgain <matthieu.bourgain@fr.clara.net>
2023-12-12 11:05:38 +0100

    add symlink creation for common-inputs.tf and common-locals.tf

--- 30_update_module.sh
+++ 30_update_module.sh
@@ -14,6 +14,37 @@ for module in $(browse_modules "$(get_scope ${1:-})" "${REPO}-*.tf"); do
     cd ${module}
     # get name of the monitors set directory
     resource="$(basename ${module})"
+    # if common files does not exist
+    if ! [ -f common-inputs.tf ]; then
+        echo -e "\t- Create common-inputs.tf symlink for module: ${module}"
+        if test -f "../../common/module/inputs.tf"; then
+            ln -s ../../common/module/inputs.tf common-inputs.tf
+        elif test -f "../../../common/module/inputs.tf"; then
+            ln -s ../../../common/module/inputs.tf common-inputs.tf
+        elif test -f "../../../../common/module/inputs.tf"; then
+            ln -s ../../../../common/module/inputs.tf common-inputs.tf
+        elif test -f "../../../../../common/module/inputs.tf"; then
+            ln -s ../../../../../common/module/inputs.tf common-inputs.tf
+        else
+            echo "didn't find inputs.tf file"
+            exit 1
+        fi
+    fi
+    if ! [ -f common-locals.tf ]; then
+        echo -e "\t- Create common-locals.tf symlink for module: ${module}"
+        if test -f "../../common/module/locals.tf"; then
+            ln -s ../../common/module/locals.tf common-locals.tf
+        elif test -f "../../../common/module/locals.tf"; then
+            ln -s ../../../common/module/locals.tf common-locals.tf
+        elif test -f "../../../../common/module/locals.tf"; then
+            ln -s ../../../../common/module/locals.tf common-locals.tf
+        elif test -f "../../../../../common/module/locals.tf"; then
+            ln -s ../../../../../common/module/locals.tf common-locals.tf
+        else
+            echo "didn't find locals.tf file"
+            exit 1
+        fi
+    fi
     # if modules.tf does not exist AND if this set respect our tagging convention
     if ! [ -f modules.tf ] && grep -q filter_tags_use_defaults inputs.tf; then
         echo -e "\t- Generate modules.tf for module: ${module}"
